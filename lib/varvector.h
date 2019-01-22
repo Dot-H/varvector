dbd5cd1257762072a6393ae1421080753e42ca19
Alexandre Bernard <alexandre.bernard@epita.fr>
2019-01-22 10:45:13 +0100

    varvector: use uint8_t and add some vec operations

--- lib/varvector.h
+++ lib/varvector.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #include <vector>
-
+#include "compact_idx.h"
 
 /*********************************************
- *          varvector implementation         *
+ *             varvector details             *
  *********************************************/
 
 namespace details {
@@ -25,7 +25,7 @@ constexpr int find_T() {
     return find_impl<0, Srch, Ts...>();
 }
 
-template <std::size_t N, typename Tuple, typename Func>
+template <std::uint8_t N, typename Tuple, typename Func>
 constexpr void foreachcont_impl(Tuple& tpl, Func fn) {
     if constexpr (N < std::tuple_size_v<Tuple>) {
         const auto& container = std::get<N>(tpl);
@@ -36,7 +36,7 @@ constexpr void foreachcont_impl(Tuple& tpl, Func fn) {
     }
 }
 
-template <std::size_t N, typename Tuple, typename Func>
+template <std::uint8_t N, typename Tuple, typename Func>
 constexpr void foreach_impl(Tuple& tpl, Func fn) {
     if constexpr (N < std::tuple_size_v<Tuple>) {
         fn(std::get<N>(tpl));
@@ -44,70 +44,113 @@ constexpr void foreach_impl(Tuple& tpl, Func fn) {
     }
 }
 
-template <std::size_t N, typename Tuple, typename Func>
-constexpr void switch_i(Tuple& tpl, std::size_t n, std::size_t i, Func fn) {
+template <std::uint8_t N, typename Tuple, typename Func>
+constexpr void switch_i(Tuple& tpl, std::uint8_t nvec, std::size_t i, Func fn) {
     if constexpr (N < std::tuple_size_v<Tuple>) {
-        if (n == N) {
+        if (nvec == N) {
             fn(std::get<N>(tpl)[i]);
         } else {
-            switch_i<N+1>(tpl, n, i, fn);
+            switch_i<N+1>(tpl, nvec, i, fn);
         }
     }
 }
 
-
 } // namespace details
+
+/*********************************************
+ *          varvector implementation         *
+ *********************************************/
 
 template <typename... Ts>
 class varvector {
 public:
-    template <typename T>
-    constexpr void push_back(T&& value) {
-        std::get<details::find_T<T, Ts...>()>(vecs).push_back(std::forward<T>(value));
-    }
 
     template <typename Func>
     constexpr void foreach(Func fn) {
-        details::foreachcont_impl<0>(vecs, fn);
+        details::foreachcont_impl<0>(_vecs, fn);
     }
 
     template <typename Func>
     constexpr void foreach(Func fn) const {
-        details::foreachcont_impl<0>(vecs, fn);
+        details::foreachcont_impl<0>(_vecs, fn);
+    }
+
+    constexpr void reserve(std::size_t n) {
+        details::foreach_impl<0>(_vecs, [&n](auto&& v) {
+            v.reserve(n);
+        });
+    }
+
+    template <typename T>
+    constexpr void push_back(T&& value) {
+        std::get<details::find_T<T, Ts...>()>(_vecs)
+            .push_back(std::forward<T>(value));
+    }
+
+    template <typename T, typename... Args>
+    constexpr void emplace_back(Args&&... args) {
+        std::get<details::find_T<T, Ts...>()>(_vecs)
+            .emplace_back(std::forward<Args>(args)...);
     }
 
     constexpr std::size_t size() const {
         std::size_t n = 0;
-        details::foreach_impl<0>(vecs, [&n](auto&& v) {
+        details::foreach_impl<0>(_vecs, [&n](auto&& v) {
             n += v.size();
         });
 
         return n;
     }
 
-    std::tuple<std::vector<Ts>...> vecs;
+    template <typename T>
+    constexpr auto back()
+        -> typename std::vector<T>::reference {
+        return std::get<details::find_T<T, Ts...>()>(varvector<Ts...>::_vecs)
+                    .back(); 
+    }
+
+    template <typename T>
+    constexpr auto back() const 
+        -> typename std::vector<T>::const_reference {
+        return std::get<details::find_T<T, Ts...>()>(varvector<Ts...>::_vecs)
+                    .back(); 
+    }
+
+protected:
+    std::tuple<std::vector<Ts>...> _vecs;
 };
+
+/*********************************************
+ *      stable_varvector implementation      *
+ *********************************************/
 
 template <typename... Ts>
 struct stable_varvector : public varvector<Ts...> {
 public:
     template <typename T>
     constexpr void push_back(T&& value) {
-        constexpr std::size_t val = details::find_T<T, Ts...>();
-        static_assert(val <= uint8_t(-1), "The vector cannot contain more than 255 types");
+        _registerPosition<T>();
+        varvector<Ts...>::push_back(std::forward<T>(value));
+    }
 
-        insertionsOrder.push_back(val);
-        std::get<val>(varvector<Ts...>::vecs).push_back(std::forward<T>(value));
+    template <typename T, typename... Args>
+    constexpr void emplace_back(Args&&... args) {
+        _registerPosition<T>();
+        varvector<Ts...>::template emplace_back<T>(std::forward<Args>(args)...);
+    }
+
+    constexpr void reserve(std::size_t n) {
+        insertionsOrder.reserve(n);
+        varvector<Ts...>::reserve(n);
     }
 
     template <typename Func>
     void foreach(Func fn) {
         std::size_t curIdx[sizeof...(Ts)] = { 0 };
 
-        for (const uint8_t i_vec : insertionsOrder) {
-            details::switch_i<0>(varvector<Ts...>::vecs,
-                                 i_vec, curIdx[i_vec], fn);
-            curIdx[i_vec]++;
+        for (const std::uint8_t i_vec : insertionsOrder) {
+            details::switch_i<0>(varvector<Ts...>::_vecs,
+                                 i_vec, curIdx[i_vec]++, fn);
         }
     }
 
@@ -115,13 +158,18 @@ public:
     void foreach(Func fn) const {
         std::size_t curIdx[sizeof...(Ts)] = { 0 };
 
-        for (const uint8_t i_vec : insertionsOrder) {
-            details::switch_i<0>(varvector<Ts...>::vecs,
-                                 i_vec, curIdx[i_vec], fn);
-            curIdx[i_vec]++;
+        for (const std::uint8_t i_vec : insertionsOrder) {
+            details::switch_i<0>(varvector<Ts...>::_vecs,
+                                 i_vec, curIdx[i_vec]++, fn);
         }
     }
 
 private:
-    std::vector<bool> insertionsOrder;
+    std::vector<std::uint8_t> insertionsOrder;
+
+    template <typename T>
+    constexpr void _registerPosition() {
+        constexpr std::uint8_t val = details::find_T<T, Ts...>();
+        insertionsOrder.push_back(val);
+    }
 };
